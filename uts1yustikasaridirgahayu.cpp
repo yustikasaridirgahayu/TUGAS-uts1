ee99c069db3f9d0527fb41d493dea2f3f50a0b17
yustikasaridirgahayu <48982094+yustukasaridirgahayu@users.noreply.github.com>
2019-03-31 21:11:52 +0800

    Add files via upload

---
+++ uts1yustikasaridirgahayu.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+struct data_mhs{
+string nama;
+string nim;
+string kelas;
+string tahun;
+};
+int main()
+{
+cout<<"nama:yusika "<<"\n";
+cout<<"nim: 1829140018"<<"\n";
+cout<<"kelas: tekom b"<<"\n";
+cout<<"tahun: 2018"<<"\n";
+return 0;}
